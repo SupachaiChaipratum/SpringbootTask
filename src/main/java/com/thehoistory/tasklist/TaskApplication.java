e7efed152da5248eea41b0fc7068a740e5c4b710
SupachaiChaipratum <ch.supachai.aon@gmail.com>
2017-09-03 16:15:36 +0700

    remove spaces

--- src/main/java/com/thehoistory/tasklist/TaskApplication.java
+++ src/main/java/com/thehoistory/tasklist/TaskApplication.java
@@ -7,11 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TaskApplication {
-	
-
 	public static void main(String[] args) {
 		SpringApplication.run(TaskApplication.class, args);
 	}
-	
-
 }
